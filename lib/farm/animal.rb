3963541c44916541270b684ff0412c77542d3a31
Daniel Hernández de León <alu0101331720@ull.edu.es>
2021-12-08 21:05:52 +0000

    Ahora se cuenta el numero de instancias de la clase Animal

--- lib/farm/animal.rb
+++ lib/farm/animal.rb
@@ -2,11 +2,15 @@ module Farm
   class Animal
     attr_accessor :name, :age, :genre, :weight
 
+    @@count = 0
+
     def initialize(name = 0, age = 0, genre = nil, weight = 0)
       @name = name
       @age = age
       @genre = genre
       @weight = weight
+
+      @@count += 1
     end
 
     def to_s

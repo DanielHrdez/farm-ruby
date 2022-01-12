6c79d43fca41e208b6ae84c99f278d24257978bc
Daniel Hernández de León <alu0101331720@ull.edu.es>
2022-01-12 21:06:16 +0000

    Las funciones ahora pueden calcular el bienestar

--- lib/farm/function.rb
+++ lib/farm/function.rb
@@ -22,5 +22,56 @@ module Farm
     def self.repro(num_meses, cattle)
       cattle.select { |animal| animal.age > (num_meses * 30) }
     end
+
+    # def self.welfare(cattle, environment)
+    #   max = 0
+    #   mean = 0
+
+    #   cattle.animals.each do |animal|
+    #     ratio = animal.weight / animal.age
+    #     if ratio > max
+    #       max = ratio
+    #     end
+    #     mean += ratio
+    #   end
+
+    #   mean /= cattle.animals.size
+
+    #   if environment == FIELD
+    #     max
+    #   else
+    #     mean * 0.5
+    #   end
+    # end
+
+    def self.my_if(condition, then_clause, else_clause)
+      if condition
+        then_clause.call
+      else
+        else_clause.call
+      end
+    end
+    
+    def self.welfare(cattle, environment)
+      self.my_if environment == FIELD,
+        -> { cattle.collect { |animal| animal.weight / animal.age }.max },
+        -> { cattle.collect { |animal| animal.weight / animal.age }.mean * 0.5 }
+    end
+
+    # def self.net_profit(cattle)
+    #   if cattle.destiny == :Sacrifice
+    #     mean = 0
+    #     cattle.animals.each do |animal|
+    #       mean += animal.weight 
+    #     end
+    #     mean *= cattle.sale_price / cattle.animals.size
+    #   else
+    #     mean = 0
+    #     cattle.animals.each do |animal|
+    #       mean += animal.age
+    #     end
+    #     mean *= cattle.sale_price / cattle.animals.size 
+    #   end
+    # end
   end
 end
